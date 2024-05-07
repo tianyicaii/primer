c354d01ea20ff6140019c869dfcb773e9a38e299
Tianyi Cai <tianyicaii@github.com>
2024-05-07 16:29:23 +0800

    add gtest fixture

--- src/hello_test.cpp
+++ src/hello_test.cpp
@@ -1,9 +1,33 @@
 #include <gtest/gtest.h>
+#include "hello.h"
 
 // Demonstrate some basic assertions.
+
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+// fixture
+
+class VecTest : public testing::Test {
+ protected:
+  VecTest() {
+     // q0_ remains empty
+     q1_.push_back(1);
+     q2_.push_back(2);
+     q2_.push_back(3);
+  }
+
+  // ~QueueTest() override = default;
+
+  std::vector<int> q0_;
+  std::vector<int> q1_;
+  std::vector<int> q2_;
+};
+
+TEST_F(VecTest, IsEmptyInitially) {
+  EXPECT_EQ(q0_.size(), 0);
 }
